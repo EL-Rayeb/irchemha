47cf9b2d54005e19a472a5abce6d5f01eb97c386
RayenMneja <rayenmneja2006@gmail.com>
2024-01-23 19:28:29 +0100

    gg

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let ScorTeam1=0;
 let ScorTeam2=0;
 let inputField = document.getElementById("team1");
 let inputField2 = document.getElementById("team2");
-
+let belote1= document.getElementById("team1");
+let belote2= document.getElementById("team2");
 const urlParams = new URLSearchParams(window.location.search);
 const name1 = urlParams.get('name1');
 const name2 = urlParams.get('name2');
@@ -10,6 +11,8 @@ const displayNameElementv = document.getElementById('v');
 const displayNameElementn = document.getElementById('n');
 displayNameElementn.textContent = name1;
 displayNameElementv.textContent = name2;
+var test1=0;
+var test2=0;
 
 
 function addToInput1(value) {
@@ -20,6 +23,15 @@ function addToInput2(value) {
   inputField2.value = value;
   inputField.value = "-";
 }
+function belotee1(){
+  test1=1
+}
+function belotee2(){
+  test2=1
+}
+
+
+
 function renderCart() {
     let cartBody = document.getElementById('cart-body');
     let team1=document.getElementById('team1').value;
@@ -42,22 +54,33 @@ function renderCart() {
       priceCell.textContent = "-";
     }
     if (team1!=="-" && team2!=="-") {
-      if ( Number(team1)%10 in [0,1,2,3,4]) {
-        teamm1=Number(team1)-(Number(team1)%10);
-        productCell.textContent = String(teamm1) ;
-      }else {
-        teamm1=Number(team1)-(Number(team1)%10)+10;
-        productCell.textContent = String(teamm1) ;
-      
-      };
-      if ( Number(team2)%10 in [0,1,2,3,4]) {
-         teamm2=Number(team2)-(Number(team2)%10);
-         priceCell.textContent = String(teamm2) ;
-      }else {
-         teamm2=Number(team2)-(Number(team2)%10)+10;
-         priceCell.textContent = String(teamm2);
-         
-      };
+      if (Number(team1)+Number(team2)<=182){
+        if ( Number(team1)%10 in [0,1,2,3,4]) {
+          teamm1=Number(team1)-(Number(team1)%10);
+          productCell.textContent = String(teamm1) ;
+        }else {
+          teamm1=Number(team1)-(Number(team1)%10)+10;
+          if(test1===1){
+            priceCell.textContent = String(teamm1+20) ;
+          }else{
+            priceCell.textContent = String(teamm1) ;
+          }
+        };
+        if ( Number(team2)%10 in [0,1,2,3,4]) {
+          teamm2=Number(team2)-(Number(team2)%10);
+          if(test2===1){
+            priceCell.textContent = String(teamm2+20) ;
+          }else{
+            priceCell.textContent = String(teamm2) ;
+          }
+          
+        }else {
+          teamm2=Number(team2)-(Number(team2)%10)+10;
+          priceCell.textContent = String(teamm2);
+        };
+      }else{
+        alert("zid thabet l7seb")
+      }
       ScorTeam1=ScorTeam1+ Number(teamm1) ;
       ScorTeam2=ScorTeam2+ Number(teamm2);
       
