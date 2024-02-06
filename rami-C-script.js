1a8e39806ea06ee789f21044a2e4cdab27af3ace
RayenMneja <rayenmneja2006@gmail.com>
2024-02-06 16:50:39 +0100

    aaa

--- rami-C-script.js
+++ rami-C-script.js
@@ -9,55 +9,15 @@ const displayNameElementv = document.getElementById('v');
 const displayNameElementn = document.getElementById('n');
 displayNameElementn.textContent = name1;
 displayNameElementv.textContent = name2;
+
+$(document).ready(function() {
+  renderCart();
+});
 function renderCart() {
-    let cartBody = document.getElementById('cart-body');
-    let team1=document.getElementById('team1').value;
-    let team2=document.getElementById('team2').value;
-    let scor1=document.getElementById('scor1');
-    let scor2=document.getElementById('scor2');
-    let row = document.createElement('tr');
-    let productCell = document.createElement('td');
-    let priceCell = document.createElement('td');
-    let affcher = "";
-    for (let i = 0; i < Number(num); i++) {
-        affcher = affcher + `<p>aaaaaaaaa</p>` ;
+    let recherche="";
+    for (let i = 0; i < num; i++) {
+      recherche+=`<div class="col-${12/Number(num)}"><div class="form-group"><input type="number" id="team${i}" class="form-control" placeholder="Team ${i} score"/></div></div>`  ;
     }
-    document.getElementById("inputscors").innerHTML=affcher;
-    if (team1==="" ||team1==="-20" && team2!=="") {
-      productCell.textContent = "-20" ;
-      priceCell.textContent = String(team2) ;
-      ScorTeam1=ScorTeam1-20 ;
-      ScorTeam2=ScorTeam2+ Number(team2);
-    }
-    if (team2==="" || team2==="-20" && team1!=="") {
-      productCell.textContent = String(team1) ;
-      priceCell.textContent = "-20" ;
-      ScorTeam1=ScorTeam1+ Number(team1) ;
-      ScorTeam2=ScorTeam2-20;
-    }
-    if (ScorTeam1>=Fscor || ScorTeam2>=Fscor) {
-        document.getElementById('name1post').value= name1;
-        document.getElementById('name2post').value= name2;
-        document.getElementById('scor1post').value= ScorTeam1;
-        document.getElementById('scor2post').value= ScorTeam2;
-        document.getElementById('time').value= new Date();
-        cartBody.appendChild(row);
-        row.appendChild(productCell);
-        row.appendChild(priceCell);
-        scor1.innerHTML=String(ScorTeam1);
-        scor2.innerHTML=String(ScorTeam2);
-        document.getElementById('team1').value="";
-        document.getElementById('team2').value="";
-        const submitButton = document.getElementById('myButton');
-        submitButton.setAttribute('style', 'display: block');
-        const irchem = document.getElementById('irchem');
-        irchem.setAttribute('style', 'display: none');
-      };
-    cartBody.appendChild(row);
-    row.appendChild(productCell);
-    row.appendChild(priceCell);
-    scor1.innerHTML=String(ScorTeam1);
-    scor2.innerHTML=String(ScorTeam2);
-    document.getElementById('team1').value="";
-    document.getElementById('team2').value="";
+    document.getElementById("inputscors").innerHTML= recherche;
+
 };
