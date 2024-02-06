2514a69421a972f30c60fec9569fe4e24c6fb3ba
RayenMneja <rayenmneja2006@gmail.com>
2024-02-06 23:47:45 +0100

    aaa ( taw tchedli zebi)

--- rami/chakanet/rami-C-script.js
+++ rami/chakanet/rami-C-script.js
@@ -13,12 +13,22 @@ displayNameElementv.textContent = name2;
 $(document).ready(function() {
   renderCart();
 });
+
 function renderCart() {
     let recherche="";
     for (let i = 0; i < num; i++) {
-      recherche+=`<div class="col-${12/Number(num)}"><div class="form-group"><input type="number" id="team${i}" class="form-control" placeholder="Team ${i} score"/></div></div>`  ;
+      recherche+=`<div class="col-${12/Number(num)}"><div class="form-group"><input type="number" id="team${i}" class="form-control" placeholder="Team ${i} score"/></div></div>`;
     }
     document.getElementById("inputscors").innerHTML= recherche;
+    let cartBody = document.getElementById('cart-body');
+    let row = document.createElement('tr');
+    let productCell = document.createElement('td');
+    cartBody.appendChild(row);
+    row.appendChild(productCell);
+    for (let j = 0; j < array.length; j++) {
+      ScorPlayers.push=document.getElementById('team${j}').value;
+      productCell.textContent=String(ScorPlayers[j]);
 
+    }
 };   
 
