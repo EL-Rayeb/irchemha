852f8c47d943ab409a596061fa8b082938b5f8b1
RayenMneja <rayenmneja2006@gmail.com>
2024-02-07 23:52:09 +0100

    fix background

--- rami/sa7aba/rami-script.js
+++ rami/sa7aba/rami-script.js
@@ -20,17 +20,34 @@ function renderCart() {
     let row = document.createElement('tr');
     let productCell = document.createElement('td');
     let priceCell = document.createElement('td');
-    if (team1==="" ||team1==="-20" && team2!=="") {
+    if (team1==="" && team2==="" || team1==="-20" && team2==="-20" || team1!=="" && team2!=="") {
+      alert("chkoun sayar lawel")
+    }
+    if ((team1==="" || team1==="-20") && team2!=="") {
       productCell.textContent = "-20" ;
       priceCell.textContent = String(team2) ;
       ScorTeam1=ScorTeam1-20 ;
       ScorTeam2=ScorTeam2+ Number(team2);
+      cartBody.appendChild(row);
+      row.appendChild(productCell);
+      row.appendChild(priceCell);
+      scor1.innerHTML=String(ScorTeam1);
+      scor2.innerHTML=String(ScorTeam2);
+      document.getElementById('team1').value="";
+      document.getElementById('team2').value="";
     }
-    if (team2==="" || team2==="-20" && team1!=="") {
+    if ((team2==="" || team2==="-20") && team1!=="") {
       productCell.textContent = String(team1) ;
       priceCell.textContent = "-20" ;
       ScorTeam1=ScorTeam1+ Number(team1) ;
       ScorTeam2=ScorTeam2-20;
+      cartBody.appendChild(row);
+      row.appendChild(productCell);
+      row.appendChild(priceCell);
+      scor1.innerHTML=String(ScorTeam1);
+      scor2.innerHTML=String(ScorTeam2);
+      document.getElementById('team1').value="";
+      document.getElementById('team2').value="";
     }
     if (ScorTeam1>=Fscor || ScorTeam2>=Fscor) {
         document.getElementById('name1post').value= name1;
@@ -49,12 +66,6 @@ function renderCart() {
         submitButton.setAttribute('style', 'display: block');
         const irchem = document.getElementById('irchem');
         irchem.setAttribute('style', 'display: none');
-      };
-    cartBody.appendChild(row);
-    row.appendChild(productCell);
-    row.appendChild(priceCell);
-    scor1.innerHTML=String(ScorTeam1);
-    scor2.innerHTML=String(ScorTeam2);
-    document.getElementById('team1').value="";
-    document.getElementById('team2').value="";
+      }
+    
 };
