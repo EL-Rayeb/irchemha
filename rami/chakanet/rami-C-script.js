be5396f9da31afd3d73229219510d7ab1bc06ab2
RayenMneja <rayenmneja2006@gmail.com>
2024-02-07 11:24:56 +0100

    rami chakanet js

--- rami/chakanet/rami-C-script.js
+++ rami/chakanet/rami-C-script.js
@@ -7,27 +7,19 @@ const Fscor = urlParams.get('Fscor');
 const num = urlParams.get('num');
 const displayNameElementv = document.getElementById('v');
 const displayNameElementn = document.getElementById('n');
-
+let team1=document.getElementById('team1').value;
 
 function renderCart() {
     let recherche="";
     for (let i = 0; i < num; i++) {
-      recherche+=`<div class="col-${12/Number(num)}"><div class="form-group"><input type="number" id="team${i}" class="form-control" placeholder="Team ${i} score"/></div></div>`;
+      recherche+=`<div class="col-${12/Number(num)}"><div class="form-group"><input type="number" id="${i}" class="form-control" placeholder="Team ${i} score"/></div></div>`;
     }
     document.getElementById("inputscors").innerHTML= recherche;
     let cartBody = document.getElementById('cart-body');
     let row = document.createElement('tr');
     let productCell = document.createElement('td');
-
-    for (let j = 0; j < Number(num); j++) {
-      ScorPlayers.push(document.getElementById('team${j}'));
-
-    }
     cartBody.appendChild(row);
     row.appendChild(productCell);
-    for (let j = 0; j < array.length; j++) {
-      ScorPlayers.push=document.getElementById('team${j}').value;
-      productCell.textContent=String(ScorPlayers[j]);
-
-    }
+    productCell.textContent=String(team1);
+    
 };
